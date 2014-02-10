82f332718d5807e370b98695c5a69afda310543e
Andrew Angell <andrew+git@angelleye.com>
2014-02-10 03:46:52 -0600

    Initial commit

---
+++ admin/footer.php
@@ -0,0 +1,17 @@
+<style type="text/css">
+<!--
+#PayPalDonate {
+	height: 47px;
+	width: 122px;
+	float: right;
+}
+-->
+</style>
+<div id="PayPalDonate">
+  <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_s-xclick" />
+    <input type="hidden" name="hosted_button_id" value="3547021" />
+    <input type="image" src="https://www.paypal.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" />
+    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+  </form>
+</div>
